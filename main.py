2e2b4336947b768cfd84e1a5431bc4294917a673
Dee <106588056+drusya-github@users.noreply.github.com>
2023-11-15 08:27:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Hello, mighty user!\n")
+#2. Ask the user for the city that they grew up in.
+cityName = input("Enter the name of the city you grew up in\n")
+#3. Ask the user for the name of a pet.
+petName = input("Enter the name of your pet\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("The name of your band could be "+cityName+" "+petName)
+
+\
